d7ccbd25d6d616a40e062cbe13ad1b72c0bd9568
Sam Hinshaw <samuel.hinshaw@abcellera.com>
2019-02-17 12:09:52 -0800

    Begin fixing tests

--- test/fetch-date-range.spec.js
+++ test/fetch-date-range.spec.js
@@ -1,11 +1,15 @@
 require('chai').should();
 const nock = require('nock');
 
-const { fetchDateRange } = require('../index.js');
+const TEST_USER = 'npmmfp';
+
+const { Session } = require('../index.js');
 
 describe('fetchDateRange', () => {
+  const session = new Session(TEST_USER);
+
   it('should be a function', () => {
-    (typeof fetchDateRange).should.equal('function');
+    (typeof session.fetchDateRange).should.equal('function');
   });
 
   it('should return an object with all available nutrient data', done => {
@@ -13,71 +17,70 @@ describe('fetchDateRange', () => {
       .get('/reports/printable_diary/npmmfp?from=2014-09-13&to=2014-09-17')
       .replyWithFile(200, `${__dirname}/mocks/diary-5Days.html`);
 
-    const expected = {
-      username: 'npmmfp',
-      data: [
-        {
-          date: '2014-09-13',
-          calories: 2078,
-          carbs: 98,
-          fat: 119,
-          protein: 153,
-          cholesterol: 1123,
-          sodium: 3031,
-          sugar: 14,
-          fiber: 5,
-        },
-        {
-          date: '2014-09-14',
-          calories: 2078,
-          carbs: 98,
-          fat: 119,
-          protein: 153,
-          cholesterol: 1123,
-          sodium: 3031,
-          sugar: 14,
-          fiber: 5,
-        },
-        {
-          date: '2014-09-15',
-          calories: 1278,
-          carbs: 39,
-          fat: 71,
-          protein: 119,
-          cholesterol: 264,
-          sodium: 2103,
-          sugar: 10,
-          fiber: 3,
-        },
-        {
-          date: '2014-09-16',
-          calories: 1470,
-          carbs: 98,
-          fat: 89,
-          protein: 71,
-          cholesterol: 979,
-          sodium: 2368,
-          sugar: 14,
-          fiber: 5,
-        },
-        {
-          date: '2014-09-17',
-          calories: 1013,
-          carbs: 5,
-          fat: 60,
-          protein: 109,
-          cholesterol: 1003,
-          sodium: 1346,
-          sugar: 4,
-          fiber: 0,
-        },
-      ],
-    };
+    const expected = [
+      {
+        date: '2014-09-13',
+        calories: 2078,
+        carbs: 98,
+        fat: 119,
+        protein: 153,
+        cholesterol: 1123,
+        sodium: 3031,
+        sugar: 14,
+        fiber: 5,
+      },
+      {
+        date: '2014-09-14',
+        calories: 2078,
+        carbs: 98,
+        fat: 119,
+        protein: 153,
+        cholesterol: 1123,
+        sodium: 3031,
+        sugar: 14,
+        fiber: 5,
+      },
+      {
+        date: '2014-09-15',
+        calories: 1278,
+        carbs: 39,
+        fat: 71,
+        protein: 119,
+        cholesterol: 264,
+        sodium: 2103,
+        sugar: 10,
+        fiber: 3,
+      },
+      {
+        date: '2014-09-16',
+        calories: 1470,
+        carbs: 98,
+        fat: 89,
+        protein: 71,
+        cholesterol: 979,
+        sodium: 2368,
+        sugar: 14,
+        fiber: 5,
+      },
+      {
+        date: '2014-09-17',
+        calories: 1013,
+        carbs: 5,
+        fat: 60,
+        protein: 109,
+        cholesterol: 1003,
+        sodium: 1346,
+        sugar: 4,
+        fiber: 0,
+      },
+    ];
 
-    fetchDateRange('npmmfp', '2014-09-13', '2014-09-17', 'all', data => {
-      data.should.deep.equal(expected);
-      done();
-    });
+    session
+      .fetchDateRange({ food: true }, '2014-09-13', '2014-09-17')
+      .then(data => {
+        data.should.deep.equal(expected);
+        done();
+      });
   });
 
   it('should return an object with only user specified nutrient data', done => {
@@ -85,26 +88,19 @@ describe('fetchDateRange', () => {
       .get('/reports/printable_diary/npmmfp?from=2014-09-13&to=2014-09-17')
       .replyWithFile(200, `${__dirname}/mocks/diary-5Days.html`);
 
-    const expected = {
-      username: 'npmmfp',
-      data: [
-        { date: '2014-09-13', calories: 2078, carbs: 98 },
-        { date: '2014-09-14', calories: 2078, carbs: 98 },
-        { date: '2014-09-15', calories: 1278, carbs: 39 },
-        { date: '2014-09-16', calories: 1470, carbs: 98 },
-        { date: '2014-09-17', calories: 1013, carbs: 5 },
-      ],
-    };
+    const expected = [
+      { date: '2014-09-13', calories: 2078, carbs: 98 },
+      { date: '2014-09-14', calories: 2078, carbs: 98 },
+      { date: '2014-09-15', calories: 1278, carbs: 39 },
+      { date: '2014-09-16', calories: 1470, carbs: 98 },
+      { date: '2014-09-17', calories: 1013, carbs: 5 },
+    ];
 
-    fetchDateRange(
-      'npmmfp',
-      '2014-09-13',
-      '2014-09-17',
-      ['calories', 'carbs'],
-      data => {
+    session
+      .fetchDateRange({ exercise: true }, '2014-09-13', '2014-09-17')
+      .then(data => {
         data.should.deep.equal(expected);
         done();
-      }
-    );
+      });
   });
 });
