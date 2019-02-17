57a8c7d2c5f3c4d3d9c1e7012e3549a87641609d
Sam Hinshaw <samuel.hinshaw@abcellera.com>
2019-02-17 08:06:21 -0800

    Add Session export

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const diaryStatusCheck = require('./mfp_functions/diary-status-check.js');
 const fetchSingleDate = require('./mfp_functions/fetch-single-date.js');
 const fetchDateRange = require('./mfp_functions/fetch-date-range.js');
 const apiStatusCheck = require('./mfp_functions/api-status-check.js');
+const Session = require('./mfp_functions/session.js');
 
 module.exports = {
   diaryStatusCheck,
   fetchSingleDate,
   fetchDateRange,
   apiStatusCheck,
+  Session,
 };
