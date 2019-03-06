47800e2e07f4e439e1b47c213f2359403170e330
Sam Hinshaw <samuel.hinshaw@abcellera.com>
2019-03-06 06:19:34 -0800

    Remove unused exports from index

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const diaryStatusCheck = require('./mfp_functions/diary-status-check.js');
-const apiStatusCheck = require('./mfp_functions/api-status-check.js');
 const Session = require('./mfp_functions/session.js');
 
 module.exports = {
-  diaryStatusCheck,
-  apiStatusCheck,
   Session,
 };
